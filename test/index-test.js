3880f316e94ab83166a785fc964a98e4f26eba50
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-01-15 03:48:49 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -77,11 +77,11 @@ function dec(n) {
   return n -= 1
 }
 
-function makeInt(n) {
-   return parseInt('a', 10)
+function makeInt(a) {
+   return parseInt('n', 10)
 }
 
-function preserveDecimal(n) {
+function preserveDecimal(a) {
   parseFloat('n')
 }
   
